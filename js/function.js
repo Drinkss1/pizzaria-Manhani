07425c0110849dec199296043869d5a8d7751b2e
Thiago Macena <thiagosantosmacena@hotmail.com>
2020-07-08 22:36:16 -0300

    colocando o site no github

---
+++ js/function.js
@@ -0,0 +1,24 @@
+$(function(){
+    $(".mobile").click(function(){
+        $(".mobile ul").slideToggle("slow");
+    });
+
+    /*var intervalo = 3000;
+
+    function slider1(){
+        document.querySelector("#img1-slider-acasa").src = "imagens/ambiente1.png";
+        setTimeout(silder2(),  3000);
+    }
+    function slider2(){
+        document.querySelector("#img1-slider-acasa").src = "imagens/ambiente2.png";
+        setTimeout(silder3(),3000);
+    }
+
+
+    function slider3(){
+        document.querySelector("#img1-slider-acasa").src = "imagens/ambiente10.png";
+        setTimeout("silder1()",3000);
+    }*/
+
+
+});
