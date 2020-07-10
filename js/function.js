a0340fb87007b71571a94ddf402b4e656145b29d
Thiago Macena <thiagosantosmacena@hotmail.com>
2020-07-10 17:33:26 -0300

    colocando outras parte do site

--- js/function.js
+++ js/function.js
@@ -22,8 +22,29 @@ $(function(){
 		$("body, html").animate({
 			//quero animar o scroll
 			scrollTop: medirOtamanho	
-		},800);
-    });
+		},1500);
+	});
+
+	$(".seta").click(function(e){
+
+		//estou dizendo que o meu menu de navegação vai cortar oq tem efeito
+		e.preventDefault();
+
+		//vou pegar a referencia do meu menu de navegação com o $ e depois pegar o atributo dele
+		var id = $(this).attr("href"),
+		//vou colocar o elemento que mede a distancia do nav aos conteudo (offset)
+			medirOtamanho = $(id).offset().top;
+			console.log(medirOtamanho);
+
+		//vou animar agora andando no meu html e body da pagina
+		$("body, html").animate({
+			//quero animar o scroll
+			scrollTop: medirOtamanho	
+		},1500);
+	});
+
+	
+
     /*fim scroll suave */
     
     /* Função para animar quando clicar contato */
